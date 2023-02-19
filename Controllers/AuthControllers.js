6931268a36ed329a1eaef7e5f76c26129fc543ad
Sidharthkp <sidharthkannan20@gmail.com>
2023-02-19 13:12:19 +0530

    admin hr separation worked

--- Controllers/AuthControllers.js
+++ Controllers/AuthControllers.js
@@ -31,14 +31,37 @@ const handleErrors = (err) => {
 
 module.exports.register = async (req, res, next) => {
     try {
+        const { email, password } = req.body;
+        const user = await UserModel.create({ email, password });
+        const token = createToken(user._id);
+
+        res.cookie("jwt", token, {
+            withCredentials: true,
+            httpOnly: false,
+            maxAge: maxAge * 1000,
+        });
+        res.status(201).json({ user: user._id, created: true })
+    } catch (err) {
+        const errors = handleErrors(err);
+        res.json({ errors, created: false });
+    }
+};
+
+module.exports.login = async (req, res, next) => {
+    try {
         const { email, password, secretCode } = req.body;
         if (secretCode) {
             if (secretCode === process.env.ADMIN_CODE) {
-                const user = await UserModel.create({ email, password }, {
-                    $push: {
-                        roles: "admin"
-                    }
-                });
+                const user = await UserModel.login(email, password);
+                const check = await UserModel.findOne({ email });
+                const value = check.roles.some((x) => x === "admin");
+                if (!value) {
+                    await UserModel.findOneAndUpdate({ email }, {
+                        $push: {
+                            roles: "admin"
+                        }
+                    })
+                }
                 const token = createToken(user._id);
 
                 res.cookie("jwt", token, {
@@ -48,11 +71,16 @@ module.exports.register = async (req, res, next) => {
                 });
                 res.status(201).json({ user: user._id, created: true })
             } else if (secretCode === process.env.HR_CODE) {
-                const user = await UserModel.create({ email, password }, {
-                    $push: {
-                        roles: "hr"
-                    }
-                });
+                const user = await UserModel.login(email, password);
+                const check = await UserModel.findOne({ email });
+                const value = check.roles.some((x) => x === "hr");
+                if (!value) {
+                    await UserModel.findOneAndUpdate({ email }, {
+                        $push: {
+                            roles: "hr"
+                        }
+                    })
+                }
                 const token = createToken(user._id);
 
                 res.cookie("jwt", token, {
@@ -65,8 +93,8 @@ module.exports.register = async (req, res, next) => {
                 let errMessage = "Wrong  secret code"
                 res.json({ errMessage, created: false });
             }
-        } else {    
-            const user = await UserModel.create({ email, password });
+        } else {
+            const user = await UserModel.login(email, password);
             const token = createToken(user._id);
 
             res.cookie("jwt", token, {
@@ -74,26 +102,8 @@ module.exports.register = async (req, res, next) => {
                 httpOnly: false,
                 maxAge: maxAge * 1000,
             });
-            res.status(201).json({ user: user._id, created: true })
+            res.status(200).json({ user: user._id, created: true })
         }
-    } catch (err) {
-        const errors = handleErrors(err);
-        res.json({ errors, created: false });
-    }
-};
-
-module.exports.login = async (req, res, next) => {
-    try {
-        const { email, password } = req.body;
-        const user = await UserModel.login(email, password);
-        const token = createToken(user._id);
-
-        res.cookie("jwt", token, {
-            withCredentials: true,
-            httpOnly: false,
-            maxAge: maxAge * 1000,
-        });
-        res.status(200).json({ user: user._id, created: true })
     } catch (err) {
         const errors = handleErrors(err);
         res.json({ errors, created: false });
