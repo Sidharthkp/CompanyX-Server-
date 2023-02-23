2b3b3b647fdfc071c2bd3b90119a4f1a312cf71d
Sidharthkp <sidharthkannan20@gmail.com>
2023-02-23 10:36:27 +0530

    commit changes

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ mongoose
         useUnifiedTopology: true,
     })
     .then(() => {
-        mongoose.set("strictQuery", false);
+        mongoose.set("strictQuery", true);
 
         const db = mongoose.connection;
 
